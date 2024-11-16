2b0d07b700ffb009a097919a94e2e2bb86b20b79
Mansi-Tanwar <tanwarmansi211@gmail.com>
2024-11-16 22:44:17 +0530

    Updated settings.py

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Project.survey',
+    'survey',
     'whitenoise.runserver_nostatic'
 ]
 
