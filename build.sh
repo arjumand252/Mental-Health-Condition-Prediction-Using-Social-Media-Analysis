b07efa79dd5f027519410d0620fd1c6f41b441d5
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-17 14:40:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 # Exit on error
 set -o errexit
 
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
 # Navigate to the 'Project' folder where manage.py is located
 cd Project
 
 # Update the app by creating survey if it's missing
 python manage.py startapp survey
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
 
 pip freeze
 
