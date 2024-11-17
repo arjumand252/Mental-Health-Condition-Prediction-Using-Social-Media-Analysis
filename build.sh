fba5fe85ffa009e4a1381b964d0fab20ae84da0b
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-17 15:00:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,12 @@ pip install -r requirements.txt
 # Navigate to the 'Project' folder where manage.py is located
 cd Project
 
+# If you're using a custom Django app packaged as a .tar.gz, install it
+# Replace this path with the actual location of your .tar.gz file
+# For example, if the .tar.gz is located at ./survey/dist/survey-package-name.tar.gz
+pip install ./survey/dist/survey-package-name.tar.gz || { echo "Failed to install survey package"; exit 1; }
+
+
 # Update the app by creating survey if it's missing
 #python manage.py startapp survey
 
