8c85d51ddfa22b992251bc5c35bbd955c2921eea
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-17 15:03:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ pip install -r requirements.txt
 # Navigate to the 'Project' folder where manage.py is located
 cd Project
 
+pip install --upgrade pip
+
 # If you're using a custom Django app packaged as a .tar.gz, install it
 # Replace this path with the actual location of your .tar.gz file
 # For example, if the .tar.gz is located at ./survey/dist/survey-package-name.tar.gz
