c0a67ad8b542860f301cf8dafaf56482a71d83ce
Mansi-Tanwar <148472134+Mansi-Tanwar@users.noreply.github.com>
2024-11-17 12:36:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd Project
 python manage.py startapp survey
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+pip install -r Project/requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
